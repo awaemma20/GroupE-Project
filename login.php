fd63a26e03ab66966caecd5f19880d60e4a8bd16
Your Name <you@example.com>
2021-03-12 09:02:52 +0000

    chnages

--- login.php
+++ login.php
@@ -21,7 +21,7 @@
       
           <button type="submit" class="btn">Login</button>
           
-         <button type="Signup" class="btn">Signup</button>
+          <button type="Signup" class="btn"><a href="registration.php">Signup</a></button>
          <a href="forgot password.html">forgot Password</a>
         </form>
       </div>
